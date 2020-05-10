e3a6d0dc0e8987938d93529652113602f49a4f6c
Edouard Goossens <edouard@woojo.ooo>
2020-05-10 20:00:28 +0200

    Export NewAlert

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,6 +3,7 @@ export * from './Bubble';
 export * from './Device';
 export * from './Friend';
 export * from './Invite';
-export * from './Notification';
+export * from './NewAlert';
 export * from './NewInviteNotification';
+export * from './Notification';
 export * from './Profile';
