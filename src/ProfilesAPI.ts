45fda38dfc7bccbe728253e4d06f026709d3227b
Edouard Goossens <edouard@woojo.ooo>
2020-05-14 18:02:57 +0200

    Add inviteLink to Profile

--- src/ProfilesAPI.ts
+++ src/ProfilesAPI.ts
@@ -5,7 +5,7 @@ export interface ProfilesAPI {
   get(uid?: string): Promise<Profile>;
   observe(uid?: string): Observable<Profile>;
   set(profile: Profile): Promise<void>;
-  update(changes: Partial<Profile>): Promise<void>;
+  update(changes: Partial<Profile>, uid?: string): Promise<void>;
   delete(uid?: string): Promise<void>;
   uidWihEmail(email: string): Promise<string | undefined>;
 }
